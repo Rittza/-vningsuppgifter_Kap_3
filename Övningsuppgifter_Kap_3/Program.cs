65f005ca5eb63a55c9dddd43a5b1b4891a3964e3
KungeniDjungeln <thhe05001@utb.vaxjo.se>
2022-09-21 11:46:35 +0200

    Add project files.

---
+++ Övningsuppgifter_Kap_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Övningsuppgfiter_kap_3
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vilken plats kom du på i den senaste idrottsturneringen du deltog i ?");
+            string plats = Console.ReadLine();
+
+            switch (plats)
+            {
+                case "1":
+                    Console.WriteLine("Du fick guld");
+                    break;
+                case "2":
+                    Console.WriteLine("Du fick silver");
+                    break;
+                case"3":
+                    Console.WriteLine("Du fick brons");
+                    break;
+                default:
+                    Console.WriteLine("Du fick tyvärr ingen medalj");
+                    break;
+            }
+        }
+    }
+}
